ab4a2e34760b3e3d22691c8fce64030a9bea24bf
Alexander Lundquist <alexander.karlsen.lundquist@gmail.com>
2026-01-30 12:58:46 +0100

    Add functionality to toggle fact form visibility and update button text

---
+++ script.js
@@ -0,0 +1,90 @@
+console.log('Hello World');
+
+const btn = document.querySelector('.btn-open')
+const form = document.querySelector('.fact-form')
+
+btn.addEventListener('click', function () {
+    if(form.classList.contains('hidden')) {
+        form.classList.remove('hidden');
+        btn.textContent = 'Close';
+    } else {
+        form.classList.add('hidden');
+        btn.textContent = 'Share a fact';
+    }
+});
+
+/*
+let votesInteresting = 23;
+let votesMindblowing = 5;
+const text = "Lisbon is the capital of Portugal."
+
+votesInteresting = votesInteresting + 1;
+votesInteresting++;
+console.log(votesInteresting);
+
+let totalUpvotes = votesInteresting + votesMindblowing
+console.log("Upvotes:", totalUpvotes);
+
+let votesFalse = 4;
+const isCorrect = votesFalse < totalUpvotes;
+console.log(isCorrect);
+
+console.log(parseInt('24.533ccc'));
+*/
+
+function calcFactAge(year) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - year;
+
+    if (age >= 0) 
+        return age;
+    else
+        return `Impossible year. Needs to be less than or equal to ${currentYear}`;
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
+console.log(calcFactAge(2020));
+console.log(calcFactAge(1990));
+console.log(calcFactAge(2037));
+
+/*
+let votesInteresting = 20;
+let votesMindblowing = 5;
+
+if (votesInteresting === votesMindblowing) {
+    alert("This fact is equally interesting and mindblowing!");
+} else if(votesInteresting > votesMindblowing){
+    console.log('Interesting Fact');
+} else if(votesInteresting < votesMindblowing) {
+    console.log('Mindblowing Fact');
+}
+
+// falsy values: 0, '', null, undefined
+// truthy value: everything else...
+if(votesMindblowing) {
+    console.log("This fact is mindblowing!");
+} else {
+    console.log('Not so special...')
+}
+
+let votesFalse = 7;
+const totalUpvotes = votesInteresting + votesMindblowing;
+
+const message = totalUpvotes > votesFalse ?
+    'The fact is true!' :
+    'Might be false, check more sources...';
+
+//alert(message);
+
+const text = "Lisbon is the capital of Portugal";
+const upperText = text.toUpperCase();
+console.log(upperText);
+
+const str = `The current fact is "${text}". It is ${
+    calcFactAge(2015)
+} years old. It is probably ${
+    totalUpvotes > votesFalse ? "correct" : "not true"
+}.`;
+console.log(str);
+*/
