7ea091e5127b06c56e043bf6f0b7e0387b1220cf
Alexander Lundquist <alexander.karlsen.lundquist@gmail.com>
2026-01-31 16:12:19 +0100

    Add initial facts data and categories, and implement fact rendering in the DOM

--- script.js
+++ script.js
@@ -1,9 +1,97 @@
-/*
-console.log('Hello World');
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+// Selecting DOM elements
 const btn = document.querySelector('.btn-open')
 const form = document.querySelector('.fact-form')
+const factsList = document.querySelector('.facts-list')
 
+// Create DOM elements: Render facts in list
+factsList.innerHTML = '';
+
+
+//Load data from Supabase
+loadFacts();
+
+async function loadFacts() {
+    const res = await fetch('https://ladfnwsrssiofdsbojeq.supabase.co/rest/v1/facts', {
+    headers: {
+        apikey: 'sb_publishable_uEtrioCkKbAmexSqwKQuDA_4xEOT20O',
+        authorization: 'Bearer sb_publishable_uEtrioCkKbAmexSqwKQuDA_4xEOT20O',
+    }
+  }
+);
+const data = await res.json();
+//console.log(data);
+//const filteredData = data.filter((fact)=>fact.category == 'technology');
+
+createFactsList(data);
+}
+
+function createFactsList(dataArray) {
+/*factsList.insertAdjacentHTML('afterbegin', "<li>Alexander is learning JavaScript!</li>");*/
+
+const htmlArr  = dataArray.map((fact)=>`<li class="fact">
+    <p>
+      ${fact.text}
+      <a 
+      class="source" 
+      href="${fact.source}" 
+      target="_blank">(Source)</a>
+    </p>
+      <span class="tag" style="background-color: 
+      ${CATEGORIES.find((cat) => cat.name === fact.category).color};">${fact.category}</span>
+    </li>`
+);
+const html = htmlArr.join('');
+factsList.insertAdjacentHTML('afterbegin', html);
+}
+
+
+
+// Toggle form visibility
 btn.addEventListener('click', function () {
     if(form.classList.contains('hidden')) {
         form.classList.remove('hidden');
@@ -13,7 +101,13 @@ btn.addEventListener('click', function () {
         btn.textContent = 'Share a fact';
     }
 });
-*/
+
+console.log([7,64,6,-23,11].filter((el) => el>10));
+console.log([7,64,6,-23,11].find((el) => el>10));
+
+
+
+
 
 /*
 let votesInteresting = 23;
@@ -97,20 +191,90 @@ const str = `The current fact is "${text}". It is ${
 }.`;
 console.log(str);
 */
-
+/*
 const fact = ['Lisbon is the capital of Portugal', 2015, true];
-console.log(fact);
 console.log(fact[2]);
 console.log(fact.length);
 console.log(fact[fact.length - 1]);
 
 const [text, createdIn] = fact;
-console.log(createdIn);
 
 const newFact = [...fact, 'society'];
 console.log(newFact);
 
-const factObj = {
+/*[2, 4, 6, 8].forEach(function(el) {
+    console.log(el);
+});
+
+/*const times10 = [2,4,6,8].map(function(el){
+    return el * 10;
+})*/
+/*
+const times10 = [2,4,6,8].map((el) => el * 10);
+console.log(times10);
+
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+const allcategories = CATEGORIES.map((el)=>el.name);
+console.log(allcategories);
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "technology",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "society",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "society",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+function calcFactAge(year) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - year;
+
+    if (age >= 0) 
+        return age;
+    else
+        return `Impossible year. Needs to be less than or equal to ${currentYear}`;
+}
+
+const factAges = initialFacts.map((el)=>calcFactAge(el.createdIn));
+console.log(factAges);
+
+/* const factObj = {
     text: 'Lisbon is the capital of Portugal',
     category: 'society',
     createdIn: 2015,
@@ -124,4 +288,4 @@ console.log(factObj['text']);
 
 const {category, isCorrect} = factObj;
 console.log(category);
-console.log(factObj.createSummary());
+console.log(factObj.createSummary()); */
